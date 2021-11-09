05c9b1389d4e1ab337e66b392726302e5bd1b8ed
mdorosz202 <mdorosz202@stevenscollege.edu>
2021-11-09 11:12:52 -0500

    Added Niko's files
    Word of warning, add the .cs file, one at a time

---
+++ FinalProject/FinalProject/Register.designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Programming1_FinalProject
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsernameRegister = new System.Windows.Forms.TextBox();
+            this.txtPasswordRegister = new System.Windows.Forms.TextBox();
+            this.lblUsernameRegister = new System.Windows.Forms.Label();
+            this.lblPasswordRegister = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUsernameRegister
+            // 
+            this.txtUsernameRegister.Location = new System.Drawing.Point(167, 168);
+            this.txtUsernameRegister.Name = "txtUsernameRegister";
+            this.txtUsernameRegister.Size = new System.Drawing.Size(384, 31);
+            this.txtUsernameRegister.TabIndex = 0;
+            // 
+            // txtPasswordRegister
+            // 
+            this.txtPasswordRegister.Location = new System.Drawing.Point(167, 294);
+            this.txtPasswordRegister.Name = "txtPasswordRegister";
+            this.txtPasswordRegister.Size = new System.Drawing.Size(384, 31);
+            this.txtPasswordRegister.TabIndex = 1;
+            // 
+            // lblUsernameRegister
+            // 
+            this.lblUsernameRegister.Location = new System.Drawing.Point(273, 112);
+            this.lblUsernameRegister.Name = "lblUsernameRegister";
+            this.lblUsernameRegister.Size = new System.Drawing.Size(224, 35);
+            this.lblUsernameRegister.TabIndex = 2;
+            this.lblUsernameRegister.Text = "Create Username:";
+            // 
+            // lblPasswordRegister
+            // 
+            this.lblPasswordRegister.Location = new System.Drawing.Point(273, 234);
+            this.lblPasswordRegister.Name = "lblPasswordRegister";
+            this.lblPasswordRegister.Size = new System.Drawing.Size(224, 35);
+            this.lblPasswordRegister.TabIndex = 3;
+            this.lblPasswordRegister.Text = "Create Password:";
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPasswordRegister);
+            this.Controls.Add(this.lblUsernameRegister);
+            this.Controls.Add(this.txtPasswordRegister);
+            this.Controls.Add(this.txtUsernameRegister);
+            this.Name = "Register";
+            this.Text = "Register";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsernameRegister;
+        private System.Windows.Forms.TextBox txtPasswordRegister;
+        private System.Windows.Forms.Label lblUsernameRegister;
+        private System.Windows.Forms.Label lblPasswordRegister;
+    }
+}
