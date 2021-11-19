96ad7a810e777dd802fe64767bbb6d6b953558f5
niko-brownlee <93388497+niko-brownlee@users.noreply.github.com>
2021-11-18 20:30:19 -0500

    New "Update" Forms + tab indexing --Niko
    Update Client Form
    Update Prescription Form
    Update Refill Form
    Tab Indexes
    All objects named appropriately for each "update" form
    Niko

---
+++ FinalProject/FinalProject/frmUpdatePrescription.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace FinalProject
+{
+    partial class frmUpdatePrescription
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpUpdatePreStartDate = new System.Windows.Forms.DateTimePicker();
+            this.label43 = new System.Windows.Forms.Label();
+            this.label39 = new System.Windows.Forms.Label();
+            this.label38 = new System.Windows.Forms.Label();
+            this.label37 = new System.Windows.Forms.Label();
+            this.btnUpdatePreUpdate = new System.Windows.Forms.Button();
+            this.btnUpdatePreClear = new System.Windows.Forms.Button();
+            this.dtpUpdatePreExpirationDate = new System.Windows.Forms.DateTimePicker();
+            this.label24 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.txtUpdatePreNumOfRefills = new System.Windows.Forms.TextBox();
+            this.txtUpdatePreMedicationID = new System.Windows.Forms.TextBox();
+            this.txtUpdatePrePhysicanID = new System.Windows.Forms.TextBox();
+            this.txtUpdatePreClientID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dtpUpdatePreStartDate
+            // 
+            this.dtpUpdatePreStartDate.Location = new System.Drawing.Point(460, 380);
+            this.dtpUpdatePreStartDate.Margin = new System.Windows.Forms.Padding(6);
+            this.dtpUpdatePreStartDate.Name = "dtpUpdatePreStartDate";
+            this.dtpUpdatePreStartDate.Size = new System.Drawing.Size(396, 31);
+            this.dtpUpdatePreStartDate.TabIndex = 3;
+            // 
+            // label43
+            // 
+            this.label43.AutoSize = true;
+            this.label43.Location = new System.Drawing.Point(320, 393);
+            this.label43.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label43.Name = "label43";
+            this.label43.Size = new System.Drawing.Size(108, 25);
+            this.label43.TabIndex = 50;
+            this.label43.Text = "Start Date";
+            // 
+            // label39
+            // 
+            this.label39.AutoSize = true;
+            this.label39.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label39.ForeColor = System.Drawing.Color.Red;
+            this.label39.Location = new System.Drawing.Point(428, 330);
+            this.label39.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label39.Name = "label39";
+            this.label39.Size = new System.Drawing.Size(23, 30);
+            this.label39.TabIndex = 49;
+            this.label39.Text = "*";
+            // 
+            // label38
+            // 
+            this.label38.AutoSize = true;
+            this.label38.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label38.ForeColor = System.Drawing.Color.Red;
+            this.label38.Location = new System.Drawing.Point(428, 230);
+            this.label38.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label38.Name = "label38";
+            this.label38.Size = new System.Drawing.Size(23, 30);
+            this.label38.TabIndex = 48;
+            this.label38.Text = "*";
+            // 
+            // label37
+            // 
+            this.label37.AutoSize = true;
+            this.label37.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label37.ForeColor = System.Drawing.Color.Red;
+            this.label37.Location = new System.Drawing.Point(428, 280);
+            this.label37.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label37.Name = "label37";
+            this.label37.Size = new System.Drawing.Size(23, 30);
+            this.label37.TabIndex = 47;
+            this.label37.Text = "*";
+            // 
+            // btnUpdatePreUpdate
+            // 
+            this.btnUpdatePreUpdate.Location = new System.Drawing.Point(280, 574);
+            this.btnUpdatePreUpdate.Margin = new System.Windows.Forms.Padding(6);
+            this.btnUpdatePreUpdate.Name = "btnUpdatePreUpdate";
+            this.btnUpdatePreUpdate.Size = new System.Drawing.Size(150, 44);
+            this.btnUpdatePreUpdate.TabIndex = 6;
+            this.btnUpdatePreUpdate.Text = "&Update";
+            this.btnUpdatePreUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdatePreClear
+            // 
+            this.btnUpdatePreClear.Location = new System.Drawing.Point(668, 574);
+            this.btnUpdatePreClear.Margin = new System.Windows.Forms.Padding(6);
+            this.btnUpdatePreClear.Name = "btnUpdatePreClear";
+            this.btnUpdatePreClear.Size = new System.Drawing.Size(150, 44);
+            this.btnUpdatePreClear.TabIndex = 7;
+            this.btnUpdatePreClear.Text = "&Clear";
+            this.btnUpdatePreClear.UseVisualStyleBackColor = true;
+            // 
+            // dtpUpdatePreExpirationDate
+            // 
+            this.dtpUpdatePreExpirationDate.Location = new System.Drawing.Point(460, 432);
+            this.dtpUpdatePreExpirationDate.Margin = new System.Windows.Forms.Padding(6);
+            this.dtpUpdatePreExpirationDate.Name = "dtpUpdatePreExpirationDate";
+            this.dtpUpdatePreExpirationDate.Size = new System.Drawing.Size(396, 31);
+            this.dtpUpdatePreExpirationDate.TabIndex = 4;
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Location = new System.Drawing.Point(256, 485);
+            this.label24.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(176, 25);
+            this.label24.TabIndex = 43;
+            this.label24.Text = "Number of Refills";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Location = new System.Drawing.Point(272, 435);
+            this.label23.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(159, 25);
+            this.label23.TabIndex = 42;
+            this.label23.Text = "Expiration Date";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(284, 335);
+            this.label21.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(143, 25);
+            this.label21.TabIndex = 41;
+            this.label21.Text = "Medication ID";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(302, 285);
+            this.label20.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(126, 25);
+            this.label20.TabIndex = 40;
+            this.label20.Text = "Physican ID";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(336, 233);
+            this.label19.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(93, 25);
+            this.label19.TabIndex = 39;
+            this.label19.Text = "Client ID";
+            // 
+            // txtUpdatePreNumOfRefills
+            // 
+            this.txtUpdatePreNumOfRefills.Location = new System.Drawing.Point(460, 480);
+            this.txtUpdatePreNumOfRefills.Margin = new System.Windows.Forms.Padding(6);
+            this.txtUpdatePreNumOfRefills.Name = "txtUpdatePreNumOfRefills";
+            this.txtUpdatePreNumOfRefills.Size = new System.Drawing.Size(196, 31);
+            this.txtUpdatePreNumOfRefills.TabIndex = 5;
+            // 
+            // txtUpdatePreMedicationID
+            // 
+            this.txtUpdatePreMedicationID.Location = new System.Drawing.Point(460, 330);
+            this.txtUpdatePreMedicationID.Margin = new System.Windows.Forms.Padding(6);
+            this.txtUpdatePreMedicationID.Name = "txtUpdatePreMedicationID";
+            this.txtUpdatePreMedicationID.Size = new System.Drawing.Size(196, 31);
+            this.txtUpdatePreMedicationID.TabIndex = 2;
+            // 
+            // txtUpdatePrePhysicanID
+            // 
+            this.txtUpdatePrePhysicanID.Location = new System.Drawing.Point(460, 280);
+            this.txtUpdatePrePhysicanID.Margin = new System.Windows.Forms.Padding(6);
+            this.txtUpdatePrePhysicanID.Name = "txtUpdatePrePhysicanID";
+            this.txtUpdatePrePhysicanID.Size = new System.Drawing.Size(196, 31);
+            this.txtUpdatePrePhysicanID.TabIndex = 1;
+            // 
+            // txtUpdatePreClientID
+            // 
+            this.txtUpdatePreClientID.Location = new System.Drawing.Point(460, 228);
+            this.txtUpdatePreClientID.Margin = new System.Windows.Forms.Padding(6);
+            this.txtUpdatePreClientID.Name = "txtUpdatePreClientID";
+            this.txtUpdatePreClientID.Size = new System.Drawing.Size(196, 31);
+            this.txtUpdatePreClientID.TabIndex = 0;
+            // 
+            // frmUpdatePrescription
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1112, 846);
+            this.Controls.Add(this.dtpUpdatePreStartDate);
+            this.Controls.Add(this.label43);
+            this.Controls.Add(this.label39);
+            this.Controls.Add(this.label38);
+            this.Controls.Add(this.label37);
+            this.Controls.Add(this.btnUpdatePreUpdate);
+            this.Controls.Add(this.btnUpdatePreClear);
+            this.Controls.Add(this.dtpUpdatePreExpirationDate);
+            this.Controls.Add(this.label24);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.txtUpdatePreNumOfRefills);
+            this.Controls.Add(this.txtUpdatePreMedicationID);
+            this.Controls.Add(this.txtUpdatePrePhysicanID);
+            this.Controls.Add(this.txtUpdatePreClientID);
+            this.Name = "frmUpdatePrescription";
+            this.Text = "Update Prescription";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpUpdatePreStartDate;
+        private System.Windows.Forms.Label label43;
+        private System.Windows.Forms.Label label39;
+        private System.Windows.Forms.Label label38;
+        private System.Windows.Forms.Label label37;
+        private System.Windows.Forms.Button btnUpdatePreUpdate;
+        private System.Windows.Forms.Button btnUpdatePreClear;
+        private System.Windows.Forms.DateTimePicker dtpUpdatePreExpirationDate;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.TextBox txtUpdatePreNumOfRefills;
+        private System.Windows.Forms.TextBox txtUpdatePreMedicationID;
+        private System.Windows.Forms.TextBox txtUpdatePrePhysicanID;
+        private System.Windows.Forms.TextBox txtUpdatePreClientID;
+    }
+}
