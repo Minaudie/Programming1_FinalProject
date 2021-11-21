607e69fa5f9115df97ed3ba43429776da6c816d2
niko-brownlee <93388497+niko-brownlee@users.noreply.github.com>
2021-11-21 10:59:24 -0500

    icon added to Client form

--- FinalProject/FinalProject/frmClient.designer.cs
+++ FinalProject/FinalProject/frmClient.designer.cs
@@ -29,6 +29,7 @@ namespace FinalProject
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmClient));
             this.txtClientUsername = new System.Windows.Forms.TextBox();
             this.txtClientPassword = new System.Windows.Forms.TextBox();
             this.lblClientUsername = new System.Windows.Forms.Label();
@@ -41,21 +42,21 @@ namespace FinalProject
             // txtClientUsername
             // 
             this.txtClientUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtClientUsername.Location = new System.Drawing.Point(81, 110);
-            this.txtClientUsername.Margin = new System.Windows.Forms.Padding(2);
+            this.txtClientUsername.Location = new System.Drawing.Point(162, 212);
+            this.txtClientUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtClientUsername.Name = "txtClientUsername";
-            this.txtClientUsername.Size = new System.Drawing.Size(218, 23);
+            this.txtClientUsername.Size = new System.Drawing.Size(432, 38);
             this.txtClientUsername.TabIndex = 0;
             this.txtClientUsername.TextChanged += new System.EventHandler(this.txtClientUsername_TextChanged);
             // 
             // txtClientPassword
             // 
             this.txtClientPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtClientPassword.Location = new System.Drawing.Point(81, 174);
-            this.txtClientPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtClientPassword.Location = new System.Drawing.Point(162, 335);
+            this.txtClientPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtClientPassword.Name = "txtClientPassword";
             this.txtClientPassword.PasswordChar = '*';
-            this.txtClientPassword.Size = new System.Drawing.Size(218, 23);
+            this.txtClientPassword.Size = new System.Drawing.Size(432, 38);
             this.txtClientPassword.TabIndex = 1;
             this.txtClientPassword.TextChanged += new System.EventHandler(this.txtClientPassword_TextChanged);
             // 
@@ -63,10 +64,10 @@ namespace FinalProject
             // 
             this.lblClientUsername.AutoSize = true;
             this.lblClientUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblClientUsername.Location = new System.Drawing.Point(78, 91);
-            this.lblClientUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblClientUsername.Location = new System.Drawing.Point(156, 175);
+            this.lblClientUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblClientUsername.Name = "lblClientUsername";
-            this.lblClientUsername.Size = new System.Drawing.Size(130, 17);
+            this.lblClientUsername.Size = new System.Drawing.Size(249, 31);
             this.lblClientUsername.TabIndex = 2;
             this.lblClientUsername.Text = "Change Username:";
             // 
@@ -74,20 +75,20 @@ namespace FinalProject
             // 
             this.lblClientPassword.AutoSize = true;
             this.lblClientPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblClientPassword.Location = new System.Drawing.Point(78, 155);
-            this.lblClientPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblClientPassword.Location = new System.Drawing.Point(156, 298);
+            this.lblClientPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblClientPassword.Name = "lblClientPassword";
-            this.lblClientPassword.Size = new System.Drawing.Size(126, 17);
+            this.lblClientPassword.Size = new System.Drawing.Size(244, 31);
             this.lblClientPassword.TabIndex = 3;
             this.lblClientPassword.Text = "Change Password:";
             // 
             // lblLandingWelcome
             // 
             this.lblLandingWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLandingWelcome.Location = new System.Drawing.Point(11, 16);
-            this.lblLandingWelcome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblLandingWelcome.Location = new System.Drawing.Point(22, 31);
+            this.lblLandingWelcome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLandingWelcome.Name = "lblLandingWelcome";
-            this.lblLandingWelcome.Size = new System.Drawing.Size(357, 48);
+            this.lblLandingWelcome.Size = new System.Drawing.Size(714, 92);
             this.lblLandingWelcome.TabIndex = 5;
             this.lblLandingWelcome.Text = "Welcome";
             this.lblLandingWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -95,9 +96,10 @@ namespace FinalProject
             // btnClientUpdate
             // 
             this.btnClientUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClientUpdate.Location = new System.Drawing.Point(106, 224);
+            this.btnClientUpdate.Location = new System.Drawing.Point(212, 431);
+            this.btnClientUpdate.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnClientUpdate.Name = "btnClientUpdate";
-            this.btnClientUpdate.Size = new System.Drawing.Size(76, 31);
+            this.btnClientUpdate.Size = new System.Drawing.Size(152, 60);
             this.btnClientUpdate.TabIndex = 6;
             this.btnClientUpdate.Text = "Update";
             this.btnClientUpdate.UseVisualStyleBackColor = true;
@@ -107,9 +109,10 @@ namespace FinalProject
             // 
             this.btnClientCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnClientCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClientCancel.Location = new System.Drawing.Point(188, 224);
+            this.btnClientCancel.Location = new System.Drawing.Point(376, 431);
+            this.btnClientCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnClientCancel.Name = "btnClientCancel";
-            this.btnClientCancel.Size = new System.Drawing.Size(76, 31);
+            this.btnClientCancel.Size = new System.Drawing.Size(152, 60);
             this.btnClientCancel.TabIndex = 6;
             this.btnClientCancel.Text = "Cancel";
             this.btnClientCancel.UseVisualStyleBackColor = true;
@@ -118,11 +121,11 @@ namespace FinalProject
             // frmClient
             // 
             this.AcceptButton = this.btnClientUpdate;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.CancelButton = this.btnClientCancel;
-            this.ClientSize = new System.Drawing.Size(379, 294);
+            this.ClientSize = new System.Drawing.Size(758, 565);
             this.Controls.Add(this.btnClientCancel);
             this.Controls.Add(this.btnClientUpdate);
             this.Controls.Add(this.lblLandingWelcome);
@@ -130,7 +133,8 @@ namespace FinalProject
             this.Controls.Add(this.lblClientUsername);
             this.Controls.Add(this.txtClientPassword);
             this.Controls.Add(this.txtClientUsername);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmClient";
             this.Text = "Client";
             this.Load += new System.EventHandler(this.frmClient_Load);
